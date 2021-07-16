6c806cf008a63cbbab6dc33f2a8d71af53e71b3c
Vlad DX <vlad.dev.experience@gmail.com>
2021-07-16 14:05:14 +0200

    Add more tests to `Xtz.StronglyTyped.SourceGenerator.IntegrationTests` - Add tests for structs with all supported inner types

--- src/Tests/Xtz.StronglyTyped.SourceGenerator.IntegrationTests/InnerTypesGeneratorTests.cs
+++ src/Tests/Xtz.StronglyTyped.SourceGenerator.IntegrationTests/InnerTypesGeneratorTests.cs
@@ -25,7 +25,7 @@ namespace Xtz.StronglyTyped.SourceGenerator.IntegrationTests
         [TestCase(typeof(uint))]
         [TestCase(typeof(ulong))]
         [TestCase(typeof(ushort))]
-        public void ShouldGenerate_ForPrimitiveTypes(Type innerType)
+        public void ShouldGenerateClass_ForPrimitiveTypes(Type innerType)
         {
             //// Arrange
 
@@ -63,7 +63,7 @@ namespace IntegrationTests.Generated
         [TestCase(typeof(MailAddress))]
         [TestCase(typeof(IPAddress))]
         [TestCase(typeof(PhysicalAddress))]
-        public void ShouldGenerate_ForKnownTypes(Type innerType)
+        public void ShouldGenerateClass_ForKnownTypes(Type innerType)
         {
             //// Arrange
 
@@ -74,6 +74,89 @@ namespace IntegrationTests.Generated
 
     [StrongType(typeof({innerType.FullName}))]
     public partial class WithInnerType
+    {{
+    }}
+}}
+            ";
+
+            var inputCompilation = CreateCompilation(sourceCode, OutputKind.DynamicallyLinkedLibrary);
+
+            //// Act
+
+            GeneratorDriver driver = CSharpGeneratorDriver.Create(new StronglyTypedGenerator());
+
+            // NOTE: the generator driver itself is immutable, and all calls return an updated version of the driver that you should use for subsequent calls
+            driver = driver.RunGeneratorsAndUpdateCompilation(inputCompilation, out var outputCompilation, out var diagnostics);
+
+            //// Assert
+
+            AssertGenerationSuccess(4, diagnostics, outputCompilation, driver.GetRunResult());
+        }
+
+        [Test]
+        [TestCase(typeof(bool))]
+        [TestCase(typeof(byte))]
+        [TestCase(typeof(char))]
+        [TestCase(typeof(decimal))]
+        [TestCase(typeof(double))]
+        [TestCase(typeof(float))]
+        [TestCase(typeof(int))]
+        [TestCase(typeof(long))]
+        [TestCase(typeof(sbyte))]
+        [TestCase(typeof(short))]
+        [TestCase(typeof(string))]
+        [TestCase(typeof(uint))]
+        [TestCase(typeof(ulong))]
+        [TestCase(typeof(ushort))]
+        public void ShouldGenerateStruct_ForPrimitiveTypes(Type innerType)
+        {
+            //// Arrange
+
+            var sourceCode = $@"
+namespace IntegrationTests.Generated
+{{
+    using Xtz.StronglyTyped.SourceGenerator;
+
+    [StrongType(typeof({innerType.FullName}))]
+    public partial struct WithInnerType
+    {{
+    }}
+}}
+            ";
+
+            var inputCompilation = CreateCompilation(sourceCode, OutputKind.DynamicallyLinkedLibrary);
+
+            //// Act
+
+            GeneratorDriver driver = CSharpGeneratorDriver.Create(new StronglyTypedGenerator());
+
+            // NOTE: the generator driver itself is immutable, and all calls return an updated version of the driver that you should use for subsequent calls
+            driver = driver.RunGeneratorsAndUpdateCompilation(inputCompilation, out var outputCompilation, out var diagnostics);
+
+            //// Assert
+
+            AssertGenerationSuccess(4, diagnostics, outputCompilation, driver.GetRunResult());
+        }
+
+        [Test]
+        [TestCase(typeof(DateTime))]
+        [TestCase(typeof(TimeSpan))]
+        [TestCase(typeof(Guid))]
+        [TestCase(typeof(Uri))]
+        [TestCase(typeof(MailAddress))]
+        [TestCase(typeof(IPAddress))]
+        [TestCase(typeof(PhysicalAddress))]
+        public void ShouldGenerateStruct_ForKnownTypes(Type innerType)
+        {
+            //// Arrange
+
+            var sourceCode = $@"
+namespace IntegrationTests.Generated
+{{
+    using Xtz.StronglyTyped.SourceGenerator;
+
+    [StrongType(typeof({innerType.FullName}))]
+    public partial struct WithInnerType
     {{
     }}
 }}
