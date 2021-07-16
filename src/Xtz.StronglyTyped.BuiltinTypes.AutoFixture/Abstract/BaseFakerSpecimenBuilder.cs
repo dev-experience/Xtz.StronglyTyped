417464cda9424d5a41c698d49df06322528a9095
Vlad DX <vlad.dev.experience@gmail.com>
2021-07-16 14:00:38 +0200

    Update `Xtz.StronglyTyped.BuiltinTypes.AutoFixture` - Add support for collection generation

--- src/Xtz.StronglyTyped.BuiltinTypes.AutoFixture/Abstract/BaseFakerSpecimenBuilder.cs
+++ src/Xtz.StronglyTyped.BuiltinTypes.AutoFixture/Abstract/BaseFakerSpecimenBuilder.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Reflection;
+using AutoFixture;
 using AutoFixture.Kernel;
 using Bogus;
 
@@ -9,18 +10,71 @@ namespace Xtz.StronglyTyped.BuiltinTypes.AutoFixture
 {
     public abstract class BaseFakerSpecimenBuilder : ISpecimenBuilder
     {
+        private static readonly int DEFAULT_REPEAT_COUNT = 3;
         private NoSpecimen NoSpecimen { get; } = new();
 
         protected abstract Dictionary<Type, Func<IFakerTInternal>> FakerFactories { get; }
 
+        // KLUDGE: Don't be surprised if you run single test without AutoData but it creates test values for other tests (which you are not trying to run at the moment)
         public object Create(object request, ISpecimenContext context)
         {
             if (request is not ParameterInfo parameterInfo)
             {
-                return NoSpecimen;
+                switch (request)
+                {
+                    case MultipleRequest {Request: SeededRequest { Request: Type multipleType }}:
+                        return CreateArray(multipleType, context);
+                    case SeededRequest { Request: Type type } when type.TryGetSingleGenericTypeArgument(typeof(IEnumerable<>), out Type? enumerableType):
+                        return CreateEnumerable(enumerableType, context);
+                    default:
+                        return NoSpecimen;
+                }
             }
 
             var parameterType = parameterInfo.ParameterType;
+
+            return Create(parameterType);
+        }
+
+        private object CreateEnumerable(Type? enumerableType, ISpecimenContext context)
+        {
+            if (enumerableType is null) return NoSpecimen;
+
+            var specimen = context.Resolve(new MultipleRequest(new SeededRequest(enumerableType, null)));
+            if (specimen is OmitSpecimen) return specimen;
+
+            var typedAdapterType = typeof(ConvertedEnumerable<>).MakeGenericType(enumerableType);
+            return Activator.CreateInstance(typedAdapterType, specimen);
+        }
+
+        private object CreateArray(Type multipleType, ISpecimenContext context)
+        {
+            var firstElement = Create(multipleType);
+            if (firstElement is NoSpecimen) return firstElement;
+
+            var count = DEFAULT_REPEAT_COUNT;
+            if (context is SpecimenContext {Builder: Fixture fixture})
+            {
+                count = fixture.RepeatCount;
+                if (count < 1)
+                {
+                    count = DEFAULT_REPEAT_COUNT;
+                }
+            }
+
+            var result = new object[count];
+            result[0] = firstElement;
+            for (var i = 1; i < count; i++)
+            {
+                result[i] = Create(multipleType);
+            }
+
+            return result;
+        }
+
+        private object Create(Type? parameterType)
+        {
+            if (parameterType is null) return NoSpecimen;
 
             var fakerObject = GetFaker(parameterType);
             if (fakerObject == null) return NoSpecimen;
---
+++ src/Xtz.StronglyTyped.BuiltinTypes.AutoFixture/Abstract/ConvertedEnumerable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Xtz.StronglyTyped.BuiltinTypes.AutoFixture
+{
+    public class ConvertedEnumerable<T> : IEnumerable<T>
+    {
+        private readonly IEnumerable<object> _enumerable;
+
+        public ConvertedEnumerable(IEnumerable<object> enumerable)
+        {
+            _enumerable = enumerable ?? throw new ArgumentNullException(nameof(enumerable));
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in _enumerable)
+            {
+                if (item is T variable) yield return variable;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
